703d55124e9c9de6fc01367701cf5a897f72f99b
joel-CM <84867719+joel-CM@users.noreply.github.com>
2023-08-22 20:05:37 -0300

    isValidWalk

--- go/main.go
+++ go/main.go
@@ -5,5 +5,6 @@ func main() {
 	// print(DuplicateEncode("Success"))
 	// println(QueueTime([]int{1, 2, 3, 4, 5}, 100))
 	// println(FindShort("hola que onda"))
-	print(DigitalRoot(493193))
+	// print(DigitalRoot(493193))
+	print(IsValidWalk([]rune{'n', 's', 'n', 's', 'n', 's', 'n', 's', 'n', 's'}))
 }
