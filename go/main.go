312c62dfa9a52b1209290fe6c7cc6a23698ca5a6
joel-CM <84867719+joel-CM@users.noreply.github.com>
2023-08-23 13:38:44 -0300

    Gap in Primes

--- go/main.go
+++ go/main.go
@@ -1,10 +1,13 @@
 package main
 
+import "fmt"
+
 func main() {
 	// print(ReverseWords("a b c "))
 	// print(DuplicateEncode("Success"))
 	// println(QueueTime([]int{1, 2, 3, 4, 5}, 100))
 	// println(FindShort("hola que onda"))
 	// print(DigitalRoot(493193))
-	print(IsValidWalk([]rune{'n', 's', 'n', 's', 'n', 's', 'n', 's', 'n', 's'}))
+	// print(IsValidWalk([]rune{'n', 's', 'n', 's', 'n', 's', 'n', 's', 'n', 's'}))
+	fmt.Printf("%v", Gap(2, 3, 50))
 }
